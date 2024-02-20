a33fdff00773df36cbdd4e6963333cbe4ffb51ee
Lajfa <dlajfa@hotmail.com>
2024-02-20 09:23:14 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const plants = document.querySelectorAll('.plant');
+
+plants.forEach((plant) => {
+  makeDraggable(plant);
+});
+
+function makeDraggable(plant, events) {
+  let offsetLeft, offsetTop, clientX, clientY;
+
+  plant.addEventListener('pointerdown', (event) => {
+    event.preventDefault();
+    offsetLeft = plant.offsetLeft;
+    offsetTop = plant.offsetTop;
+    clientX = event.clientX;
+    clientY = event.clientY;
+    document.addEventListener('pointermove', movePlant);
+  });
+
+  document.addEventListener('pointerup', () => {
+    document.removeEventListener('pointermove', movePlant);
+  });
+
+  function movePlant(event) {
+    const dx = event.clientX - clientX;
+    const dy = event.clientY - clientY;
+    plant.style.left = offsetLeft + dx + 'px';
+    plant.style.top = offsetTop + dy + 'px';
+  }
+}
